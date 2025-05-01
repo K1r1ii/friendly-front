ba1dab6615ac31cb89bfda3cbbd72116bf133e37
Kirill <my@ksalnikov.ru>
2025-05-01 18:17:03 +0300

    add api and components folders

---
+++ src/api/fetchData.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'http://localhost:8000'; //todo: поменять после деплоя бэка
+
+/*
+Общая функция для получения данных от API 
+*/
+export async function fetchData(endpoint, options = {}) {
+  const response = await fetch(`${BASE_URL}/${endpoint}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers
+    },
+    ...options
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+
+  return await response.json();
+}
